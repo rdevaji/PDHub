e7c36a9854fbde4ab3ee5496b37307ff9cde9f93
rdevaji <62391432+rdevaji@users.noreply.github.com>
2020-03-19 11:38:08 -0400

    Fixed script

--- signin.js
+++ signin.js
@@ -50,7 +50,7 @@ $('#inputPassword').keypress(function(e) {
 });
 
 //Exercise Pt2 - FIX ME!
-$('').on('', function() {
+$('#signin-button').on('click', function() {
     if (email != $('#inputEmail').val()) count = 0;
     count++;
     email = $('#inputEmail').val();
